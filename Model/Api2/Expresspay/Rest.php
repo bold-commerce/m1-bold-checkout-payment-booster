4942b1dd20dc04cf6e72278bfb8f40997a3a6d8b
Joseph Leedy <joseph.leedy@boldcommerce.com>
2024-09-27 13:53:59 -0600

    Create API endpoint for creating Express Pay order (CHK-5019)

---
+++ Model/Api2/Expresspay/Rest.php
@@ -0,0 +1,68 @@
+<?php
+
+class Bold_CheckoutPaymentBooster_Model_Api2_Expresspay_Rest extends Bold_CheckoutPaymentBooster_Model_Api2_Expresspay
+{
+    /**
+     * Creates an Express Pay order in Bold Checkout
+     */
+    protected function _create()
+    {
+        $quoteId = $this->getRequest()->getParam('quoteId');
+        $gatewayId = $this->getRequest()->getParam('gatewayId');
+        /** @var Mage_Sales_Model_Quote $quote */
+        $quote = Mage::getModel('quote/quote')->load($quoteId);
+
+        if ($quote->getId() === null) {
+            return [
+                'order_id' => null,
+                'error' => Mage::helper('core')
+                    ->__('Could not create Express Pay order. Invalid quote ID "%s".', $quoteId)
+            ];
+        }
+
+        $websiteId = $quote->getStore()->getWebsiteId();
+        $uri = '/checkout/orders/{{shopId}}/wallet_pay';
+        $quoteConverter = new Bold_CheckoutPaymentBooster_Service_ExpressPay_QuoteConverter();
+        $expressPayData = $quoteConverter->convertFullQuote($quote, $gatewayId);
+
+        try {
+            $result = Bold_CheckoutPaymentBooster_Service_Client::post($uri, $websiteId, $expressPayData);
+        } catch (Mage_Core_Exception $exception) {
+            return [
+                'order_id' => null,
+                'error' => Mage::helper('core')
+                    ->__('Could not create Express Pay order. Express Pay order. Error: "%s"', $exception->getMessage())
+            ];
+        }
+
+        if (property_exists($result, 'errors') && count($result->errors) > 0) {
+            if (is_array($result->errors[0])) {
+                $exceptionMessage = Mage::helper('core')
+                    ->__(
+                        'Could not create Express Pay order. Errors: "%s"',
+                        implode(', ', array_column($result->errors, 'message'))
+                    );
+            } else {
+                $exceptionMessage = Mage::helper('core')
+                    ->__('Could not create Express Pay order. Error: "%s"', $result->errors[0]);
+            }
+
+            return [
+                'order_id' => null,
+                'error' => $exceptionMessage
+            ];
+        }
+
+        if (!property_exists($result, 'body') || count($result->body) === 0) {
+            return [
+                'order_id' => null,
+                'error' => Mage::helper('core')->__('An unknown error occurred while creating the Express Pay order.')
+            ];
+        }
+
+        return [
+            'order_id' => $result->body['data']['order_id'],
+            'error' => null
+        ];
+    }
+}
