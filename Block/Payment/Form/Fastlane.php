6a09facd02f91cbca09563884c465e596dca9184
NickolasMalovanets <149711420+NickolasMalovanets@users.noreply.github.com>
2024-10-14 11:58:20 +0300

    M1 make Fastlane to use Payments SDK for init. (#54)
    * M1 make Fastlane to use Payments SDK for init.
    
    * M1 make Fastlane to use Payments SDK for init.
    
    * M1 make Fastlane to use Payments SDK for init.

--- Block/Payment/Form/Fastlane.php
+++ Block/Payment/Form/Fastlane.php
@@ -3,87 +3,18 @@
 /**
  * Bold payment method block.
  */
-class Bold_CheckoutPaymentBooster_Block_Payment_Form_Fastlane extends Mage_Payment_Block_Form
+class Bold_CheckoutPaymentBooster_Block_Payment_Form_Fastlane extends
+    Bold_CheckoutPaymentBooster_Block_Payment_Form_Base
 {
-    /**
-     * @var Mage_Sales_Model_Quote|null
-     */
-    private $quote = null;
-
     /**
      * @inheritDoc
      */
     protected function _construct()
     {
         parent::_construct();
-        $this->quote = Mage::getSingleton('checkout/session')->getQuote();
-        if ($this->isAvailable()) {
+        if ($this->isFastlaneAvailable()) {
             $this->setTemplate('bold/checkout_payment_booster/payment/form/bold_fastlane_method.phtml');
         }
-    }
-
-    /**
-     * Retrieve Fastlane email container styles.
-     *
-     * @return string
-     */
-    public function getWatermarkContainerStyle()
-    {
-        $websiteId = $this->quote->getStore()->getWebsiteId();
-        /** @var Bold_CheckoutPaymentBooster_Model_Config $config */
-        $config = Mage::getSingleton(Bold_CheckoutPaymentBooster_Model_Config::RESOURCE);
-        return $config->getFastlaneWatermarkContainerStyles($websiteId);
-    }
-
-    /**
-     * Check if fastlane payment method is available.
-     *
-     * @return int
-     */
-    public function isAvailable()
-    {
-        /** @var Bold_CheckoutPaymentBooster_Model_Payment_Fastlane $fastlane */
-        $fastlane = Mage::getModel('bold_checkout_payment_booster/payment_fastlane');
-        return (int)$fastlane->isAvailable($this->quote);
-    }
-
-    /**
-     * Get public order ID.
-     *
-     * @return string
-     */
-    public function getPublicOrderId()
-    {
-        return Bold_CheckoutPaymentBooster_Service_Bold::getPublicOrderId();
-    }
-
-    /**
-     * Get address container style.
-     *
-     * @return string
-     */
-    public function getAddressContainerStyle()
-    {
-        $websiteId = $this->quote->getStore()->getWebsiteId();
-        /** @var Bold_CheckoutPaymentBooster_Model_Config $config */
-        $config = Mage::getSingleton(Bold_CheckoutPaymentBooster_Model_Config::RESOURCE);
-        return $config->getFastlaneAddressContainerStyles($websiteId);
-    }
-
-    /**
-     * Get EPS gateway ID from Bold checkout data.
-     *
-     * @return string|null
-     */
-    public function getEpsGatewayId()
-    {
-        $boldCheckoutData = Bold_CheckoutPaymentBooster_Service_Bold::getBoldCheckoutData();
-        if (!$boldCheckoutData) {
-            return null;
-        }
-        return isset($boldCheckoutData->flow_settings->eps_gateway_id)
-            ? $boldCheckoutData->flow_settings->eps_gateway_id
-            : null;
     }
 
     /**
@@ -96,28 +27,5 @@ class Bold_CheckoutPaymentBooster_Block_Payment_Form_Fastlane extends Mage_Payme
         $fastlaneStyles = Bold_CheckoutPaymentBooster_Service_Bold::getFastlaneStyles();
         $fastlaneStyles = $fastlaneStyles ? (array)$fastlaneStyles : [];
         return json_encode($fastlaneStyles);
-    }
-
-    /**
-     * Retrieve EPS URL.
-     *
-     * @return string
-     */
-    public function getEpsUrl()
-    {
-        $websiteId = $this->quote->getStore()->getWebsiteId();
-        /** @var Bold_CheckoutPaymentBooster_Model_Config $config */
-        $config = Mage::getSingleton(Bold_CheckoutPaymentBooster_Model_Config::RESOURCE);
-        return $config->getEpsUrl($websiteId) . '/' . $config->getShopDomain($websiteId) . '/';
-    }
-
-    /**
-     * Retrieve EPS auth token.
-     *
-     * @return string|null
-     */
-    public function getEpsAuthToken()
-    {
-        return Bold_CheckoutPaymentBooster_Service_Bold::getEpsAuthToken();
     }
 }
---
+++ Service/Order/Hydrate/ExtractData.php
@@ -0,0 +1,272 @@
+<?php
+
+/**
+ * Extract data from quote.
+ */
+class Bold_CheckoutPaymentBooster_Service_Order_Hydrate_ExtractData
+{
+    // list of system totals which need to be skipped during preparing discounts and fees list
+    const SYSTEM_TOTALS = [
+        'subtotal',
+        'tax',
+        'discount',
+        'shipping',
+        'grand_total',
+    ];
+
+    private static $requiredFields = [
+        'city',
+        'firstname',
+        'street',
+        'lastname',
+        'telephone',
+        'postcode',
+        'region',
+        'region_id',
+    ];
+
+    private static $discounts = [];
+
+    private static $fees = [];
+
+    /**
+     * Extract quote data into array.
+     *
+     * @param Mage_Sales_Model_Quote $quote
+     * @return array
+     */
+    public static function extractQuoteData(Mage_Sales_Model_Quote $quote)
+    {
+        $quote->collectTotals();
+        self::getDiscountsAndFees($quote);
+        $quoteData = [
+            'customer' => self::getCustomer($quote),
+            'billing_address' => self::convertQuoteAddress($quote, 'billing'),
+            'cart_items' => self::getCartItems($quote),
+            'taxes' => self::getTaxes($quote),
+            'discounts' => self::$discounts,
+            'fees' => self::$fees,
+            'totals' => self::getTotals($quote),
+        ];
+        if (!$quote->isVirtual()) {
+            $quoteData['shipping_address'] = self::convertQuoteAddress($quote, 'shipping');
+            $quoteData['shipping_line'] = self::getShippingLine($quote);
+        }
+
+        return $quoteData;
+    }
+
+    /**
+     * Retrieve customer data.
+     *
+     * @param Mage_Sales_Model_Quote $quote
+     * @return array
+     */
+    private static function getCustomer(Mage_Sales_Model_Quote $quote)
+    {
+        return [
+            'platform_id' => $quote->getCustomerId() ? (string)$quote->getCustomerId() : null,
+            'first_name' => $quote->getCustomerFirstname(),
+            'last_name' => $quote->getCustomerLastname(),
+            'email_address' => $quote->getCustomerEmail(),
+        ];
+    }
+
+    /**
+     * Convert quote billing|shipping address to appropriate format.
+     *
+     * @param Mage_Sales_Model_Quote $quote
+     * @return array
+     */
+    private static function convertQuoteAddress(Mage_Sales_Model_Quote $quote, $type)
+    {
+        $billingAddress = $quote->getbillingAddress();
+        $shippingAddress = $quote->getShippingAddress();
+        $address = $type === 'billing' ? $billingAddress : $shippingAddress;
+        foreach (self::$requiredFields as $field) {
+            if (!$address->getData($field)) {
+                $address = $billingAddress->getData($field) ? $billingAddress : $shippingAddress;
+                break;
+            }
+        }
+        $countryIsoCode = Mage::getModel('directory/country')
+            ->load($address->getCountryId())
+            ->getIso2Code();
+        $countryName = Mage::app()
+            ->getLocale()
+            ->getCountryTranslation($countryIsoCode);
+
+        $addressData = [
+            'first_name' => $address->getFirstname(),
+            'last_name' => $address->getLastname(),
+            'business_name' => $address->getCompany() ? $address->getCompany() : '',
+            'phone_number' => $address->getTelephone(),
+            'address_line_1' => $address->getStreet1(),
+            'address_line_2' => $address->getStreet2(),
+            'city' => $address->getCity(),
+            'country' => $countryName,
+            'country_code' => $address->getCountryId(),
+            'postal_code' => $address->getPostcode(),
+        ];
+
+        $province = $address->getRegion();
+        if (!empty($province)) {
+            $addressData['province'] = $province;
+        }
+
+        $provinceCode = $address->getRegionCode();
+        if (!empty($provinceCode)) {
+            $addressData['province_code'] = $provinceCode;
+        }
+
+        return $addressData;
+    }
+
+    /**
+     * Retrieve cart items.
+     *
+     * @return array
+     */
+    private static function getCartItems(Mage_Sales_Model_Quote $quote)
+    {
+        $items = [];
+        foreach ($quote->getAllVisibleItems() as $item) {
+            $items[] = Bold_CheckoutPaymentBooster_Service_Quote_Item::extract($item);
+        }
+
+        return $items;
+    }
+
+    /**
+     * Retrieve taxes.
+     *
+     * @return array
+     */
+    private static function getTaxes(Mage_Sales_Model_Quote $quote)
+    {
+        $totals = $quote->getTotals();
+        $taxInfo = isset($totals['tax']['full_info']) ? $totals['tax']['full_info'] : [];
+        $taxes = [];
+        foreach ($taxInfo as $tax) {
+            $taxes[] = [
+                'name' => $tax['id'],
+                'value' => self::convertToCents($tax['base_amount']),
+            ];
+        }
+
+        return $taxes;
+    }
+
+    /**
+     * Retrieve shipping line.
+     *
+     * @return array
+     */
+    private static function getShippingLine(Mage_Sales_Model_Quote $quote)
+    {
+        if ($quote->isVirtual()) {
+            return [];
+        }
+
+        return [
+            'rate_name' => $quote->getShippingAddress()->getShippingDescription() ?: '',
+            'cost' => self::convertToCents($quote->getShippingAddress()->getShippingAmount()),
+        ];
+    }
+
+    /**
+     * Retrieve discounts and fees.
+     *
+     * @param Mage_Sales_Model_Quote $quote
+     * @return void
+     */
+    private static function getDiscountsAndFees(Mage_Sales_Model_Quote $quote)
+    {
+        $totals = $quote->getTotals();
+
+        if (isset($totals['discount'])) {
+            self::$discounts[] = [
+                'line_text' => isset($totals['discount']['title']) ? $totals['discount']['title'] : '',
+                'value' => abs(self::convertToCents($totals['discount']['value'])),
+            ];
+        }
+
+        foreach ($totals as $total) {
+            if (in_array($total['code'], self::SYSTEM_TOTALS) || !$total['value']) {
+                continue;
+            }
+
+            $title = isset($total['title'])
+                ? $total['title']
+                : ucfirst(str_replace('_', ' ', $total['code']));
+
+            if ($total['value'] > 0) {
+                self::$fees[] = [
+                    'description' => $title,
+                    'value' => self::convertToCents($total['value']),
+                ];
+            } else {
+                self::$discounts[] = [
+                    'line_text' => $title,
+                    'value' => abs(self::convertToCents($total['value'])),
+                ];
+            }
+        }
+    }
+
+    /**
+     * Retrieve totals.
+     *
+     * @return array
+     */
+    private static function getTotals(Mage_Sales_Model_Quote $quote)
+    {
+        $totals = $quote->getTotals();
+        $subTotal = isset($totals['subtotal']['value_excl_tax'])
+            ? $totals['subtotal']['value_excl_tax']
+            : $totals['subtotal']['value'];
+        $shippingTotal = isset($totals['shipping']['value_excl_tax'])
+            ? $totals['shipping']['value_excl_tax']
+            : $totals['shipping']['value'];
+        $grandTotal = isset($totals['grand_total']['value_incl_tax'])
+            ? $totals['grand_total']['value_incl_tax']
+            : $totals['grand_total']['value'];
+        $processedTotals = [
+            'sub_total' => self::convertToCents($subTotal),
+            'tax_total' => isset($totals['tax']['value']) && $totals['tax']['value'] ? self::convertToCents($totals['tax']['value']) : 0,
+            'discount_total' => self::getDiscountTotal(),
+            'shipping_total' => self::convertToCents($shippingTotal),
+            'order_total' => self::convertToCents($grandTotal),
+        ];
+        $calculatedGrandTotal = $processedTotals['sub_total'] + $processedTotals['tax_total']
+            + $processedTotals['shipping_total'] - $processedTotals['discount_total'];
+        if ($calculatedGrandTotal > $processedTotals['order_total']
+            && $calculatedGrandTotal === $processedTotals['order_total'] + $processedTotals['tax_total']) {
+            $processedTotals['order_total'] = $calculatedGrandTotal;
+        }
+        return $processedTotals;
+    }
+
+    /**
+     * Retrieve discount total.
+     *
+     * @return int
+     */
+    private static function getDiscountTotal()
+    {
+        return !empty(self::$discounts)
+            ? array_sum(array_column(self::$discounts, 'value'))
+            : 0;
+    }
+
+    /**
+     * Convert an amount to cents.
+     *
+     * @param float|string $amount
+     * @return integer
+     */
+    private static function convertToCents($amount)
+    {
+        return (int)round(floatval($amount) * 100);
+    }
+}
