548fc793e4828e2cc4d45a759b0455d79a93bf24
jeremywiebeofbold <141751891+jeremywiebeofbold@users.noreply.github.com>
2024-12-10 10:34:06 -0600

    CHK-6850 Remove hydrate order calls (#71)
    - The hydrateOrderData calls have been broken for an unknown amount of time.
    - These calls appeared to succeed, but inspection of their response payload revealed
      a stack trace resulting from a server-side exception being thrown.
    - The trace indicated that the expected call to sidekick's hydrate endpoint was
      never occuring.
    - All other aspects of the M1 Bold Booster appeared to still work correctly, including
      order completion, payment capture and the order update in Checkout.
    - Removed the `hydrateOrderData` functions from the Bold Booster frontend and backend code.
    - Removed `subscribeToBillingAddressUpdate` because it only called `hydrateOrderData`.
    - Tested with Bold Booster using PPCP (acdc and PayPal button) and Fastlane.
    
    [Fixes CHK-6850]

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -6,36 +6,6 @@
 class Bold_CheckoutPaymentBooster_IndexController extends Mage_Core_Controller_Front_Action
 {
     /**
-     * Hydrate order data to Bold order action.
-     *
-     * @return void
-     */
-    public function hydrateOrderDataAction()
-    {
-        if (!$this->_validateFormKey()) {
-            return;
-        }
-        $post = $this->getRequest()->getPost();
-        unset($post['form_key']);
-        if (empty($post)) {
-            Mage::throwException('No data provided.');
-        }
-        $quote = Mage::getSingleton('checkout/session')->getQuote();
-        if (!$quote->getId()) {
-            Mage::throwException('No quote found.');
-        }
-        if (isset($post['address_id']) && $quote->getCustomer()->getId()) {
-            Bold_CheckoutPaymentBooster_Service_Order_Hydrate::hydrate($quote);
-            return;
-        }
-        $post['street'] = $post['street2']
-            ? $post['street1'] . "\n" . $post['street2']
-            : $post['street1'];
-        $this->addAddressDataToQuote($quote, $post);
-        Bold_CheckoutPaymentBooster_Service_Order_Hydrate::hydrate($quote);
-    }
-
-    /**
      * Get cart data action.
      *
      * @return void
